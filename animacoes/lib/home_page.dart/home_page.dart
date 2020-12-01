34e28365e21b0689e8a043c4161fb2f98592b221
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-11-30 21:13:58 -0300

    Animações Explícitas

--- animacoes/lib/home_page.dart/home_page.dart
+++ animacoes/lib/home_page.dart/home_page.dart
@@ -10,6 +10,53 @@ class _HomePageState extends State<HomePage> {
   var endSize = 100.0;
   bool isGrowing = false;
 
+  //Essa não é uma forma muito adequada de gerenciar
+  //o estado desta aplicação, pois temos muita 
+  //repetição de código nos 2 estados. 
+
+  Widget _stateNormal(){
+    //O Flutter reaproveita a key na árvore de elementos
+    //de forma que mesmo que 2 AnimatedContainer
+    //diferentes sejam renderizados, eles sejam o mesmo
+    return AnimatedContainer(
+      //se não passarmos o atributo key o flutter 
+      //gera automaticamente ele, e gerará o mesmo 
+      //key para esse animated container ou o do 
+      //state grow pois será chamado no mesmo local
+      //Se passarmos, se forem iguais surtirão efeito
+      //no mesmo widget, se forem diferentes o 
+      //flutter entenderá que são 2 widgets 
+      //diferentes logo não terá + interpolação
+      // key: ValueKey("key"),
+      //Para garantir a mesma instância de 2 widgets
+      //é só eles terem a mesma key.
+      transform: Matrix4.identity()..translate(0.0, 0.0),
+      duration: Duration(seconds: 2),
+      width: startSize,
+      height: startSize,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(40),
+        color: Colors.red
+      )
+    );
+  }
+
+  Widget _stateGrow(){
+    //O AnimatedContainer serve para animações 
+    //explícitas, ou seja, que criamos na mão.
+    return AnimatedContainer(
+      // key: ValueKey("key 2"),
+      transform: Matrix4.identity()..translate(0.0, -300.0),
+      duration: Duration(seconds: 2),
+      width: endSize,
+      height: endSize,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(40),
+        color: Colors.red
+      )
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -23,26 +70,7 @@ class _HomePageState extends State<HomePage> {
         child: Icon(Icons.refresh),
       ),
       body: Center(
-        //animações ímplicitas = tudo já está sendo
-        //feito internamente. Há vários widgets desse
-        //que se iniciam com Animated
-        child: AnimatedContainer(
-          // o Matrix4.identity deixa identado 
-          //no centro do widget de layout pai, no
-          //caso o center, então esse elemento
-          //será identado ao centro da tela
-          //Uma forma de transladar:
-          transform: Matrix4.identity()..translate(0.0, -300.0),
-          //usamos o .. para de uma mesma instância executar
-          //2 métodos.
-          duration: Duration(seconds: 2),
-          width: isGrowing ? endSize : startSize,
-          height: isGrowing ? endSize : startSize,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(40),
-            color: Colors.red
-          )
-        ),
+        child: isGrowing ? _stateGrow() : _stateNormal(),
       )
     );
   }
